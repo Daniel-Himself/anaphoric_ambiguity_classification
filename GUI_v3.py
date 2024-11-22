95ada6b92e627eeaad45262fc1cc214938bd052f
Daniel Sharon <danielsharon97@gmail.com>
2024-11-22 16:58:55 +0200

    Removed duplicate pronounes

--- GUI_v3.py
+++ GUI_v3.py
@@ -198,9 +198,9 @@ def main():
     print("NLP model loaded successfully.")
 
     # Define pronouns
-    pronouns = ["I", "me", "my", "mine", "myself", "you", "you", "your", "yours", "yourself", 
-                "he", "him", "his", "his", "himself", "she", "her", "her", "hers", "herself", 
-                "it", "it", "its", "itself", "we", "us", "our", "ours", "ourselves", "you", 
+    pronouns = ["I", "me", "my", "mine", "myself", "you", "your", "yours", "yourself", 
+                "he", "him", "his", "himself", "she", "her", "hers", "herself", 
+                "it", "its", "itself", "we", "us", "our", "ours", "ourselves", 
                 "you", "your", "yours", "yourselves", "they", "them", "their", "theirs", "themselves"]
 
     # Setup GUI
